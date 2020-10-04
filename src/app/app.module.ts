c9af3e3ad5e04cf880ce32338a5e1565042aa1a6
David Lara Diaz <david.lara01@uptc.edu.co>
2020-10-04 08:44:42 -0500

    menu and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/plantilla/menu/menu.component';
+import { FooterComponent } from './components/plantilla/footer/footer.component';
+import { InformeComponent } from './components/informe/informe.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { PropietarioComponent } from './components/propietario/propietario.component';
+import { VehiculoComponent } from './components/vehiculo/vehiculo.component';
+import { ConductorComponent } from './components/conductor/conductor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    InformeComponent,
+    RegistroComponent,
+    PropietarioComponent,
+    VehiculoComponent,
+    ConductorComponent
   ],
   imports: [
     BrowserModule,
